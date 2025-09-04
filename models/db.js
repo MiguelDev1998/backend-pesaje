f73987dda4d74429ace801e550351ba25ccef4a3
Miguel <miguel.199835@gmail.com>
2025-09-04 02:10:05 -0600

    0.2

--- models/db.js
+++ models/db.js
@@ -1,20 +1,25 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT, // Asegúrate de tenerlo en el .env si usas Railway
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-connection.connect((err) => {
+// Prueba de conexión (opcional)
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('❌ Error de conexión a MySQL:', err);
   } else {
     console.log('✅ Conexión exitosa a la base de datos MySQL');
+    connection.release(); // Muy importante liberar la conexión
   }
 });
 
-module.exports = connection;
-
+module.exports = pool;
