a546b1e380d9e629c3a8d30785666dab5ea8e194
Miguel <miguel.199835@gmail.com>
2025-10-01 20:14:52 -0600

    PILOTOS CRUD WEB 4.0

--- models/piloto.model.js
+++ models/piloto.model.js
@@ -3,27 +3,27 @@ const db = require('./db');
 
 const Piloto = {
   getAll: (callback) => {
-    db.query('SELECT * FROM pilotos', callback);
+    db.query('SELECT * FROM piloto', callback);
   },
 
   getById: (id, callback) => {
-    db.query('SELECT * FROM pilotos WHERE id = ?', [id], callback);
+    db.query('SELECT * FROM piloto WHERE id = ?', [id], callback);
   },
 
   create: (data, callback) => {
-    db.query('INSERT INTO pilotos (nombre, licencia, telefono) VALUES (?, ?, ?)', 
+    db.query('INSERT INTO piloto (nombre, licencia, telefono) VALUES (?, ?, ?)', 
       [data.nombre, data.licencia, data.telefono], callback);
   },
 
   update: (id, data, callback) => {
     db.query(
-      'UPDATE pilotos SET nombre = ?, licencia = ?, telefono = ? WHERE id = ?', 
+      'UPDATE piloto SET nombre = ?, licencia = ?, telefono = ? WHERE id = ?', 
       [data.nombre, data.licencia, data.telefono, id], callback
     );
   },
 
   delete: (id, callback) => {
-    db.query('DELETE FROM pilotos WHERE id = ?', [id], callback);
+    db.query('DELETE FROM piloto WHERE id = ?', [id], callback);
   }
 };
 
